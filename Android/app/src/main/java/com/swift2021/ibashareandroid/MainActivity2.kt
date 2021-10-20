1104e43ad9d895eb7a1055b2e0d5d1e80703ce2e
azusa1120 <azunanashiee@gmail.com>
2021-10-20 17:10:56 +0900

    画面遷移

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity2.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity2.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toNextPage:Button = findViewById(R.id.toNextPage)
-        toNextPage.setOnClickListener {
+        setContentView(R.layout.activity_main2)
+        val toBackPage: Button = findViewById(R.id.toBackPage)
+        toBackPage.setOnClickListener {
             // 起動する対象をクラスオブジェクトで指定する
-            val intent = Intent(this, MainActivity2::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
