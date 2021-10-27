ee24b8d9df61403a47d9ec4497d9a0a72de95667
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2021-10-27 11:03:05 +0900

    画面遷移ができた

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
@@ -2,10 +2,22 @@ package com.swift2021.ibashareandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.media.Image
+import  android.widget.Button
+import  android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image1 :ImageButton = findViewById(R.id.image1)
+
+        image1.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
     }
 }
