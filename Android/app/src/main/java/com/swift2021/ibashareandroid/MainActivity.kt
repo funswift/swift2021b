1e1187d3ceb80d2d2dd23f0b9eae18ef8fa77a0d
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2021-10-28 01:57:25 +0900

    画面遷移後の文字の受け渡しが完了

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
@@ -13,17 +13,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image1 :ImageButton = findViewById(R.id.image1)
-        val image2 :ImageButton = findViewById(R.id.image2)
-        val image3 :ImageButton = findViewById(R.id.image3)
-        val image4 :ImageButton = findViewById(R.id.image4)
-        val placeName1:TextView = findViewById(R.id.place1)
+        val image1: ImageButton = findViewById(R.id.image1)
+        val image2: ImageButton = findViewById(R.id.image2)
+        val image3: ImageButton = findViewById(R.id.image3)
+        val image4: ImageButton = findViewById(R.id.image4)
+
+        val placeName1: TextView = findViewById(R.id.place1)
+        val placeName2: TextView = findViewById(R.id.place2)
+        val placeName3: TextView = findViewById(R.id.place3)
+        val placeName4: TextView = findViewById(R.id.place4)
 
 
-        image1.setOnClickListener{
+
+        image1.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
 
-            intent.putExtra("PlaceName", placeName1.text.toString() )
+            intent.putExtra("PlaceName", placeName1.text.toString())
+            startActivity(intent)
+        }
+
+        image2.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra("PlaceName", placeName2.text.toString())
+            startActivity(intent)
+        }
+
+        image3.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra("PlaceName", placeName3.text.toString())
+            startActivity(intent)
+        }
+
+        image4.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra("PlaceName", placeName4.text.toString())
             startActivity(intent)
         }
 
