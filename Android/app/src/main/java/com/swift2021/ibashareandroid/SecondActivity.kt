732daf8b2d41a31f8d74d8401fc2faea3e5c49a0
azusa1120 <azunanashiee@gmail.com>
2021-10-27 11:32:32 +0900

    他の町を見るボタン(1)

--- Android/app/src/main/java/com/swift2021/ibashareandroid/SecondActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/SecondActivity.kt
@@ -1,13 +1,10 @@
 package com.swift2021.ibashareandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_townlist.*
 import android.view.View
 import android.widget.TextView
-
-
-
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_townlist.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -18,24 +15,32 @@ class SecondActivity : AppCompatActivity() {
         // Intent を取得する
         val intent = intent
         // キーを元にデータを取得する
-        val text = intent.getIntExtra("town", 1)
-        val txt = findViewById<View>(R.id.textview) as TextView
+        val text = intent.getIntExtra("town",1)
 
-        when (text) {
-            1 -> town1.text
-            2 -> town2.text
-            3 -> town3.text
-            4 -> town4.text
-            5 -> town5.text
-            6 -> town6.text
-            7 -> town7.text
-            8 -> town8.text
-            9 -> town9.text
-            10 -> town10.text
-            11 -> town11.text
-            12 -> town12.text
-            13 -> town13.text
+//        when (text) {
+//            1 -> town1.text
+//            2 -> town2.text
+//            3 -> town3.text
+//            4 -> town4.text
+//            5 -> town5.text
+//            6 -> town6.text
+//            7 -> town7.text
+//            8 -> town8.text
+//            9 -> town9.text
+//            10 -> town10.text
+//            11 -> town11.text
+//            12 -> town12.text
+//            13 -> town13.text
+//        }
+
+        val returnButton: TextView = findViewById(R.id.textview)
+
+        returnButton.setOnClickListener {
+            finish()
         }
-        txt.text = text.toString()
+    }
+    // 戻るボタン
+    fun onBackButtonTapped(view: View?) {
+        finish()
     }
 }
