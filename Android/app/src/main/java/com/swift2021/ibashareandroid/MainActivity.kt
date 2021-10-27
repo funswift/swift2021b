aad152cb39f91f9ecb0158f116650669f84baf43
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2021-10-28 01:43:16 +0900

    画面遷移後の文字の受け渡しが一部完了

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.media.Image
 import  android.widget.Button
 import  android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val image1 :ImageButton = findViewById(R.id.image1)
+        val image2 :ImageButton = findViewById(R.id.image2)
+        val image3 :ImageButton = findViewById(R.id.image3)
+        val image4 :ImageButton = findViewById(R.id.image4)
+        val placeName1:TextView = findViewById(R.id.place1)
+
 
         image1.setOnClickListener{
             val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra("PlaceName", placeName1.text.toString() )
             startActivity(intent)
         }
 
--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity2.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity2.kt
@@ -2,10 +2,17 @@ package com.swift2021.ibashareandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val placeName:TextView = findViewById(R.id.placeName)
+
+        val getPlaceName = intent.getStringExtra("PlaceName")
+        placeName.text = getPlaceName
+
     }
 }
