ce06c9526f802600d2c024c4c1bf853926f93f8d
Yuto5176 <yuuhanasaki39@gmail.com>
2021-11-02 02:47:57 +0900

    画像と文字切り替え

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.swift2021.ibashareandroid
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var linearLayout: LinearLayout? = null
+    private var imageNames = arrayOf(
+        R.drawable.fun_entaku,
+        R.drawable.fun_entaku,
+        R.drawable.fun_entaku,
+        R.drawable.fun_entaku
+    )
+    private var textNames = arrayOf("亀田町", "美原町", "富岡町", "赤川町")
+    private var aryIndex = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        initButton()
     }
+
+    private fun initView() {
+        val imageView: ImageView = findViewById(R.id.imageViewMain)
+        val textView: TextView = findViewById(R.id.textViewMain)
+
+        imageView.setImageResource(imageNames[aryIndex])
+        textView.text = textNames[aryIndex]
+    }
+
+    private fun initButton() {
+        val displayButton: Button = findViewById(R.id.button)
+        displayButton.setOnClickListener {
+            if (aryIndex < textNames.size) {
+                aryIndex = (aryIndex + 1) % textNames.size
+                imageChange()
+            }
+        }
+    }
+
+    private fun imageChange() {
+        val imageView: ImageView = findViewById(R.id.imageViewMain)
+        val textView: TextView = findViewById(R.id.textViewMain)
+        val countView: TextView = findViewById(R.id.count)
+
+        imageView.setImageResource(imageNames[aryIndex])
+        textView.text = textNames[aryIndex]
+        countView.text = ("" + (aryIndex + 1))
+    }
+
 }
