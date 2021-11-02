e4134a3db57ad17fde4e2ed3607e1e61f37868f9
azusa1120 <azunanashiee@gmail.com>
2021-11-02 19:57:25 +0900

    Activityのリファクタリング

--- Android/app/src/main/java/com/swift2021/ibashareandroid/TownListActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/TownListActivity.kt
@@ -28,81 +28,80 @@ class TownListActivity : AppCompatActivity() {
         val button13 = findViewById<Button>(R.id.town13)
 
         button1.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town1Activity::class.java)
-            intent.putExtra("town", 1)
-            town1.text
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button1.text.toString())
             startActivity(intent)
         }
 
         button2.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town2Activity::class.java)
-            intent.putExtra("town", 2)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button2.text.toString())
             startActivity(intent)
         }
 
         button3.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town3Activity::class.java)
-            intent.putExtra("town", 3)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button3.text.toString())
             startActivity(intent)
         }
 
         button4.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town4Activity::class.java)
-            intent.putExtra("town", 4)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button4.text.toString())
             startActivity(intent)
         }
 
         button5.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town5Activity::class.java)
-            intent.putExtra("town", 5)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button5.text.toString())
             startActivity(intent)
         }
 
         button6.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town6Activity::class.java)
-            intent.putExtra("town", 6)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button6.text.toString())
             startActivity(intent)
         }
 
         button7.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town7Activity::class.java)
-            intent.putExtra("town", 7)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button7.text.toString())
             startActivity(intent)
         }
 
         button8.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town8Activity::class.java)
-            intent.putExtra("town", 8)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button8.text.toString())
             startActivity(intent)
         }
 
         button9.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town9Activity::class.java)
-            intent.putExtra("town", 9)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button9.text.toString())
             startActivity(intent)
         }
 
         button10.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town10Activity::class.java)
-            intent.putExtra("town", 10)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button10.text.toString())
             startActivity(intent)
         }
 
         button11.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town11Activity::class.java)
-            intent.putExtra("town", 11)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button11.text.toString())
             startActivity(intent)
         }
 
         button12.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town12Activity::class.java)
-            intent.putExtra("town", 12)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button12.text.toString())
             startActivity(intent)
         }
 
         button13.setOnClickListener {
-            val intent = Intent(this@TownListActivity, Town13Activity::class.java)
-            intent.putExtra("town", 13)
+            val intent = Intent(this@TownListActivity, TownActivity::class.java)
+            intent.putExtra("town", button13.text.toString())
             startActivity(intent)
         }
     }
