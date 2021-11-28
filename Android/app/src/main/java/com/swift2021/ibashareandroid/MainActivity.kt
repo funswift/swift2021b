9e0f2a03bf9cfcf7a5ae651f0c41b69ced4629eb
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2021-11-29 06:25:04 +0900

    編み物属性と関数を追加

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
@@ -184,4 +184,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
 }
--- Android/app/src/main/java/com/swift2021/ibashareandroid/model/User.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/model/User.kt
@@ -4,6 +4,7 @@ class User(val name:String, val townToLive:String) {
 
     var Shogi:Int? = null
     var Igo:Int? = null
+    var Amimono:Int? = null
 
     fun tapShogi(){
         Igo = +1
@@ -11,6 +12,10 @@ class User(val name:String, val townToLive:String) {
 
     fun tapIgo(){
         Shogi = +1
+    }
+
+    fun tapAmimono(){
+        Amimono = +1
     }
 
 }
