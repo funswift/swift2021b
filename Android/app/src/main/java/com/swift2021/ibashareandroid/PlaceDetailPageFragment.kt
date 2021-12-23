8775dcc3b6722cfa33526c39b7d9ecfb31eedd2b
Yuto5176 <yuuhanasaki39@gmail.com>
2021-12-24 05:49:24 +0900

    トップ画面と詳細画面のナビゲーション

--- Android/app/src/main/java/com/swift2021/ibashareandroid/PlaceDetailPageFragment.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/PlaceDetailPageFragment.kt
@@ -1,17 +1,52 @@
 package com.swift2021.ibashareandroid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_see_more_tag1.*
+import kotlinx.android.synthetic.main.fragment_place_detail_page.*
 
 class PlaceDetailPageFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_place_detail_page, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        when (0) {
+            0 -> imageView.setImageResource(R.mipmap.templete)
+            1 -> imageView.setImageResource(R.mipmap.shogi01)
+            2 -> imageView.setImageResource(R.mipmap.shogi02)
+            3 -> imageView.setImageResource(R.mipmap.shogi03)
+            4 -> imageView.setImageResource(R.mipmap.shogi04)
+        }
+
+        seeMoreKutikomiButton.setOnClickListener {
+
+        }
+        seeMoreParticipantButton.setOnClickListener {  }
+
+        button_send.setOnClickListener {
+            findNavController().navigate(R.id.action_place_detail_to_top)
+        }
+
+
+
+    }
+
 
 }
