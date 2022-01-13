4243dbccafe14c7cd58f5b110da81595bab223f4
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-01-13 14:58:15 +0900

    リストビューの作成

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MyPageActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MyPageActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MyPageActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +34,15 @@ class MyPageActivity : AppCompatActivity() {
         navigation_icon_my_page.setColorFilter(ContextCompat.getColor(this, R.color.orange), PorterDuff.Mode.SRC_IN)
         navigation_icon_top.setColorFilter(ContextCompat.getColor(this, R.color.gray_74), PorterDuff.Mode.SRC_IN)
         navigation_icon_my_page_name.setTextColor(getColor(R.color.orange))
+
+        val myGenreList = mutableListOf("将棋","囲碁","編み物","料理")
+
+        val listView = findViewById<ListView>(R.id.list_view)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myGenreList)
+
+        listView.adapter = adapter
+
 
     }
 }
