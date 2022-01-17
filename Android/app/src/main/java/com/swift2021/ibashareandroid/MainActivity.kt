22de1e912c4e7cbe7b618c7c3ddcdbe72bfd2382
Yuto5176 <yuuhanasaki39@gmail.com>
2022-01-17 14:13:42 +0900

    ナビゲーションバーと各fragmentの連携

--- Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
+++ Android/app/src/main/java/com/swift2021/ibashareandroid/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.swift2021.ibashareandroid
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -11,22 +18,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        naviMovement()
+        bottom_navigation.setupWithNavController((supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController)
+
+//        naviMovement()
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment).commit()
+//        supportFragmentManager.beginTransaction().hide(currentFragment).commit()
+//        supportFragmentManager.beginTransaction().show(fragment).commit()
+        currentFragment = fragment
+    }
+
+    private val fragmentList = mutableListOf(TopPageFragment(), SearchPageFragment(), TalkPageFragment(), MyPageFragment())
+    private var currentFragment = fragmentList[0]
 
     private fun naviMovement(){
         bottom_navigation.setOnItemSelectedListener { item ->
             when(item.itemId){
-                R.id.navi_top -> {
+                R.id.topPageFragment -> {
+                    replaceFragment(fragmentList[0])
                     return@setOnItemSelectedListener true
                 }
-                R.id.navi_search -> {
+                R.id.searchPageFragment -> {
+                    replaceFragment(fragmentList[1])
                     return@setOnItemSelectedListener true
                 }
-                R.id.navi_talk -> {
+                R.id.talkPageFragment -> {
+                    replaceFragment(fragmentList[2])
                     return@setOnItemSelectedListener true
                 }
-                R.id.navi_my_page -> {
+                R.id.myPageFragment -> {
+                    replaceFragment(fragmentList[3])
                     return@setOnItemSelectedListener true
                 }
             }
